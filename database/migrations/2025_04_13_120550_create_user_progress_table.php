f7f41fc9f5b971c70f90309c25617a1e7dac9c73
Ali Aljawad <alialjawad2k1@gmail.com>
2025-06-17 23:18:41 +0300

    Add timestamps to submissions, user progress, and user stats tables; remove last_updated field from UserProgressSeeder

--- database/migrations/2025_04_13_120550_create_user_progress_table.php
+++ database/migrations/2025_04_13_120550_create_user_progress_table.php
@@ -12,13 +12,13 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('user_progress', function (Blueprint $table) {
+            $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->foreignId('level_id')->constrained()->onDelete('cascade');
             $table->boolean('is_completed')->default(false);
             $table->integer('score')->default(0);
             $table->integer('attempts')->default(0);
-            $table->timestamp('last_updated')->useCurrent()->useCurrentOnUpdate();
-            $table->primary(['user_id', 'level_id']);
+            $table->timestamps();
         });
 
     }
